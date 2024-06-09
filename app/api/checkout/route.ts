05ddce4d166c9c5ea05ab187b8ca40f4b6211db5
Yeuda Shtern <yeudashtern@gmail.com>
2024-06-09 13:44:03 +0300

    rr

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe"
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin":  `${process.env.ECOMMERCE_STORE_URL}`|| "*",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
